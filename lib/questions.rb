d4b33bc1da4eb7d0ae11fd3b4e76e23438f1e311
Jonny Pickard <jonathandpickard@gmail.com>
2016-05-20 18:32:44 +0100

    10 Cut string in half and return first half

--- lib/questions.rb
+++ lib/questions.rb
@@ -49,10 +49,11 @@ def array_sort_by_last_letter_of_word(array)
   array.sort_by { | word | word[-1] }
 end
 
-# cut strings in half, and return the first half, e.g.
+# 10 Cut strings in half, and return the first half, e.g.
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+  string.slice( 0, ( string.length / 2.to_f ).ceil )
 end
 
 # turn a positive integer into a negative integer. A negative integer
