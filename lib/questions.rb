0192483880239b4a7065bb90cc876dfab0618aed
Jonny Pickard <jonathandpickard@gmail.com>
2016-05-22 16:24:21 +0100

    19 Get the average from an array rounded to the nearest integer

--- lib/questions.rb
+++ lib/questions.rb
@@ -112,9 +112,10 @@ def turn_symbol_into_string(symbol)
   symbol.to_s
 end
 
-# get the average from an array, rounded to the nearest integer
+# 19 Get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 17
 def average_of_array(array)
+  (array.reduce(:+)/array.length.to_f).round
 end
 
 # get all the elements in an array, up until the first element
